b961dbbcd392d942a899263cc1825c1124d78980
Sonali Gupta <97590082+sona-123@users.noreply.github.com>
2022-10-07 17:25:34 +0530

    Add files via upload

---
+++ sonic/script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Sargam");
+let songindex=0;
+let audioelement=
+let songs=[
+    {songname: "Ishq De Fanniyar", filepath: "songs1.mp3"},
+    {songname: "Ishq De Fanniyar", filepath: "songs1.mp3"},
+    {songname: "Ishq De Fanniyar", filepath: "songs1.mp3"},
+    {songname: "Ishq De Fanniyar", filepath: "songs1.mp3"},
+    {songname: "Ishq De Fanniyar", filepath: "songs1.mp3"},
+]
+
+let audiofile=new Audio('song.mp3');
+document.addEventListener('time')
